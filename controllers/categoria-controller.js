b18968c165d8564b672df30e39dd7e77ecf2afa0
Rodrigo Klaes <rodrigoklaes@gmail.com>
2018-09-25 16:55:30 -0300

    Ajustes no controller da Categoria

--- controllers/categoria-controller.js
+++ controllers/categoria-controller.js
@@ -1,36 +1,35 @@
-'use strict'
-require('../models/categoria-model');
-const repository = require('../repositories/categoria-repository')
+"use strict";
+require("../models/categoria-model");
+const repository = require("../repositories/categoria-repository");
 
-function CategoriaController() {
+class CategoriaController {
+  constructor() {}
 
-}
-
-CategoriaController.prototype.post = async (req, res) => {
-    let result = await new repository().create(req.body)
+  async post(req, res) {
+    let result = await new repository().create(req.body);
     res.status(201).send(result);
-};
+  }
 
-CategoriaController.prototype.put = async (req, res) => {
+  async put(req, res) {
     let result = await new repository().update(req.params.id, req.body);
     res.status(202).send(result);
-};
+  }
 
-CategoriaController.prototype.get = async (req, res) => {
+  async get(req, res) {
     let result = await new repository().find();
     res.status(200).send(result);
-};
+  }
 
-CategoriaController.prototype.getById = async (req, res) => {
+  async getById(req, res) {
     let result = await new repository().findById(req.params.id);
     res.status(200).send(result);
-};
+  }
 
-CategoriaController.prototype.delete = async (req, res) => {
+  async delete(req, res) {
     let obj = new repository();
     let result = await obj.findById(req.params.id);
-    obj.delete(req.params.id);    
+    obj.delete(req.params.id);
     res.status(200).send(result);
-};
-
+  }
+}
 module.exports = CategoriaController;
